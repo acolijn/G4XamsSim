846a165f28b926ad650d77aeb20c3c0775ffdbd3
Auke Pieter Colijn <acolijn@users.noreply.github.com>
2024-09-09 17:31:35 +0200

    Update DetectorConstructionMessenger.hh

--- include/DetectorConstructionMessenger.hh
+++ include/DetectorConstructionMessenger.hh
@@ -14,6 +14,13 @@ namespace G4Sim {
 
 class DetectorConstruction;
 
+/**
+ * @class DetectorConstructionMessenger
+ * @brief A class responsible for handling user commands related to the detector construction.
+ *
+ * This class inherits from G4UImessenger and provides methods to set new values for various parameters of the detector construction.
+ * The class is used to interact with the user interface and update the corresponding values in the DetectorConstruction class.
+ */
 class DetectorConstructionMessenger : public G4UImessenger {
     public:
         DetectorConstructionMessenger(DetectorConstruction* detector);
